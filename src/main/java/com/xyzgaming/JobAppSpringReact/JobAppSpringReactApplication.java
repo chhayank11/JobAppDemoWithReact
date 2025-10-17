68565a172ebd3b04f76504fb6861a689c7c3a4fc
Chhayank <cdixit1313@gmail.com>
2025-10-18 01:32:46 +0530

    initial commit

---
+++ src/main/java/com/xyzgaming/JobAppSpringReact/JobAppSpringReactApplication.java
@@ -0,0 +1,13 @@
+package com.xyzgaming.JobAppSpringReact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAppSpringReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobAppSpringReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xyzgaming/JobAppSpringReact/JobAppSpringReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyzgaming.JobAppSpringReact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAppSpringReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
